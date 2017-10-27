344e80181e2a698735c51931544253b4231eab83
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-10-28 00:08:04 +0800

    Change output filetype to csv

--- gen_gene_info.sh
+++ gen_gene_info.sh
@@ -2,7 +2,8 @@
 set -ueo pipefail
 
 # input file is somethine like 'gencode.v27.basic.annotation.gtf.gz'
+echo -e "seqname,source,start,end,strand,gene_id,gene_type,gene_name"
 zcat "$1" \
     | grep -v '^#' \
-    | awk 'BEGIN{OFS="\t"}$3=="gene"{print $1, $2, $4, $5, $7, $10, $12, $14}' \
+    | awk 'BEGIN{OFS=","}$3=="gene"{print $1, $2, $4, $5, $7, $10, $12, $14}' \
     | tr -d '";'
